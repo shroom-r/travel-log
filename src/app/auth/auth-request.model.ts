f209cec1baf672c0b9008b671ee293294966b45c
Rafael T <68326435+shroom-r@users.noreply.github.com>
2023-06-26 18:06:06 +0200

    Fixed login page component

--- src/app/auth/auth-request.model.ts
+++ src/app/auth/auth-request.model.ts
@@ -1,4 +1,4 @@
-export class AuthRequest {
-  username?: string;
-  password?: string;
+export type AuthRequest = {
+  username: string;
+  password: string;
 }
--- src/app/auth/login-page/login-page.component.ts
+++ src/app/auth/login-page/login-page.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from "@angular/core";
-import { AuthRequest } from "../auth-request.model";
-import { AuthService } from "../auth.service";
-import { Router } from "@angular/router";
+import { Component } from "@angular/core";
 import { NgForm } from "@angular/forms";
+import { Router } from "@angular/router";
+import { AuthService } from "../auth.service";
+import { AuthRequest } from "../auth-request.model";
 
 @Component({
   selector: "app-login-page",
@@ -13,18 +13,20 @@ export class LoginPageComponent {
   /**
    * This authentication request object will be updated when the user
    * edits the login form. It will then be sent to the API.
+   *
+   * Partial is a generic typescript that creates a type from the type param,
+   * with all the properties optionally undefined.
    */
-  authRequest: AuthRequest;
+  authRequestInput: Partial<AuthRequest>;
 
   /**
-   * If true, it means that the authentication API has return a failed response
+   * If defined, it means that the authentication API has return a failed response
    * (probably because the name or password is incorrect).
    */
-  loginError: boolean;
+  loginError?: string;
 
   constructor(private auth: AuthService, private router: Router) {
-    this.authRequest = new AuthRequest();
-    this.loginError = false;
+    this.authRequestInput = {};
   }
 
   /**
@@ -34,16 +36,21 @@ export class LoginPageComponent {
     // Only do something if the form is valid
     if (form.valid) {
       // Hide any previous login error.
-      this.loginError = false;
+      this.loginError = undefined;
 
       // Perform the authentication request to the API.
-      this.auth.login$(this.authRequest).subscribe({
-        next: () => this.router.navigateByUrl("/"),
-        error: (err) => {
-          this.loginError = true;
-          console.warn(`Authentication failed: ${err.message}`);
-        },
-      });
+      // Since the login$() method requires an AuthRequest param, but
+      // our authRequestInput has optional properties, we need to convert it
+      // to an new object that matches the AuthRequest type.
+      this.auth
+        .login$({
+          password: this.authRequestInput.password ?? "",
+          username: this.authRequestInput.username ?? "",
+        })
+        .subscribe({
+          next: () => this.router.navigateByUrl("/"),
+          error: (err) => (this.loginError = err.message),
+        });
     }
   }
 }
