bd33e672a24fbe9da672b529edf33bf6bf4f94c1
Alexander Mussitelli <alexander.mussitelli@gmail.com>
2023-07-02 17:07:57 +0200

    import of FormsModule for later - review of course 25 May, 1h51

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -33,7 +34,8 @@ import { ListTripsAndPlacesComponent } from './list-trips-and-places/list-trips-
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    AuthModule
+    AuthModule,
+    FormsModule,
   ],
   providers: [
     {
