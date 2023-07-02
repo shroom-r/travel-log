67af613d62bcfd6388e46441b7fb363e2e0cb3d9
Rafael T <68326435+shroom-r@users.noreply.github.com>
2023-07-02 20:11:55 +0200

    Added trip-update-request type + added services to delete trip, search by val, get trip by ID and update trip

---
+++ src/app/trips/trip-update-request.model.ts
@@ -0,0 +1,4 @@
+export type TripUpdateRequest = {
+  title?: string,
+  description?: string,
+}
--- src/app/trips/trip.service.ts
+++ src/app/trips/trip.service.ts
@@ -4,6 +4,7 @@ import { TripCreationRequest } from './trip-creation-request.model';
 import { Observable, tap } from 'rxjs';
 import { TripResponse } from './trip-response.model';
 import { environment } from 'src/environments/environment';
+import { TripUpdateRequest } from './trip-update-request.model';
 
 @Injectable({
   providedIn: 'root',
@@ -12,6 +13,7 @@ export class TripService {
   apiUrlTrips = `${environment.apiUrl}/trips`;
   constructor(private http: HttpClient) {}
 
+  //create a new trip
   createTrip(
     tripCreationRequest: TripCreationRequest
   ): Observable<TripResponse> {
@@ -24,9 +26,58 @@ export class TripService {
       );
   }
 
+  //Delete a trip
+  deleteTrip(tripId: string) {
+    return this.http.delete(this.apiUrlTrips + `/${tripId}`).pipe(
+      tap((reponse) => {
+        return Response;
+      })
+    );
+  }
+
+  //Get all trips for one user
   getUserTrips(userId: string): Observable<TripResponse[]> {
     return this.http
       .get<TripResponse[]>(this.apiUrlTrips + `?user=${userId}`)
+      .pipe(
+        tap((response) => {
+          return response;
+        })
+      );
+  }
+
+  searchTripBySearchValue(searchValue: string): Observable<TripResponse[]> {
+    //Build querystring
+    //Split searchValue and pass it to array
+    var searchValuesArray = [];
+    searchValuesArray = searchValue.split(' ');
+    //Loop through array to build query string. Delete last '&' after loop completed
+    var queryString = '?';
+    for (let value of searchValuesArray) {
+      queryString += `search=${value}&`;
+    }
+    queryString = queryString.slice(0, -1);
+    return this.http.get<TripResponse[]>(this.apiUrlTrips + queryString).pipe(
+      tap((response) => {
+        return response;
+      })
+    );
+  }
+
+  getTripById(tripId: string): Observable<TripResponse> {
+    return this.http.get<TripResponse>(this.apiUrlTrips + `/${tripId}`).pipe(
+      tap((response) => {
+        return response;
+      })
+    );
+  }
+
+  updateTrip(
+    tripId: string,
+    tripUpdate: TripUpdateRequest
+  ): Observable<TripResponse> {
+    return this.http
+      .patch<TripResponse>(this.apiUrlTrips + `/${tripId}`, tripUpdate)
       .pipe(
         tap((response) => {
           return response;
