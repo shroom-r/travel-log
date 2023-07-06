7162c2f6312b755801ed90b5b04d1aa184fb4afc
Alexander Mussitelli <alexander.mussitelli@gmail.com>
2023-07-06 19:22:14 +0200

    add service get trips/places in tiles. Some styling. button for detail

--- src/app/all-my-trips-page/tile/tile.component.ts
+++ src/app/all-my-trips-page/tile/tile.component.ts
@@ -1,4 +1,16 @@
 import { Component } from '@angular/core';
+import { TripResponse } from '../../trips/trip-response.model';
+import { PlaceResponse } from '../../places/place-response.model';
+import { TripService } from '../../trips/trip.service';
+import { AuthService } from '../../auth/auth.service';
+import { PlacesService } from '../../places/places.service';
+import { Router } from '@angular/router';
+
+
+type PlacesByTrip = {
+  trip?: TripResponse;
+  places?: PlaceResponse[];
+};
 
 @Component({
   selector: 'app-tile',
@@ -11,10 +23,67 @@ export class TileComponent {
   // on doit afficher au minimum un titre et une description
   title: string;
   tripDescription: string;
+  tripsList: TripResponse[] = [];
+  placesList: PlaceResponse[] = [];
+  placesByTrip: PlacesByTrip[] = [];
 
-  constructor() {
+  constructor(    private tripService: TripService,
+    private auth: AuthService,
+    private placesService: PlacesService,
+    private router: Router
+    ) {
     this.title = 'titre Tuile';
     this.tripDescription = 'Description du trip';
+    this.getTripsList();
   }
 
+  seeDetail(tripId? :string) {
+    this.router.navigate(["tripDetail/"+tripId]);
+  }
+
+  getTripsList() {
+    this.auth.getUserId().subscribe((userId) => {
+      if (userId) {
+        this.tripService.getUserTrips(userId).subscribe((trips) => {
+          this.tripsList = trips;
+          this.mapTripsToPlacesByTrip();
+          this.getPlaces();
+        });
+      }
+    });
+  }
+
+  getPlaces() {
+    for (let trip of this.tripsList) {
+      var tripId = trip.id;
+      this.placesService.getPlacesOfTrip(tripId).subscribe((places) => {
+        this.mapPlacesToPlacesByTrip(places);
+      });
+    }
+  }
+
+  mapTripsToPlacesByTrip() {
+    for (let trip of this.tripsList) {
+      //push every trip to placesByTrip
+      this.placesByTrip.push({ trip: trip });
+    }
+  }
+
+  mapPlacesToPlacesByTrip(placesArray: PlaceResponse[]) {
+    var tripId: string;
+    var index: number;
+    for (let place of placesArray) {
+      //For each place in placesArray
+      //Get trip id
+      tripId = place.tripId;
+      //get index of corresponding trip
+      index = this.placesByTrip.map((el) => el.trip?.id).indexOf(tripId);
+      //create array for places if it doesn't exist
+      if (!this.placesByTrip[index].places) {
+        this.placesByTrip[index].places = [];
+      }
+      //adds places to corresponding trip
+      this.placesByTrip[index].places?.push(place);
+    }
+  }
 }
