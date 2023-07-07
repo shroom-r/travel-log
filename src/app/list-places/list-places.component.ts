3c839789a8af70e088e7cc14fbd7ea2abfff6f35
Rafael T <68326435+shroom-r@users.noreply.github.com>
2023-07-07 19:06:48 +0200

    Working on list-places component of page tripDetail

--- src/app/list-places/list-places.component.ts
+++ src/app/list-places/list-places.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
+import { PlaceResponse } from '../places/place-response.model';
+import { TripResponse } from '../trips/trip-response.model';
+import { PlacesService } from '../places/places.service';
 
 @Component({
   selector: 'app-list-places',
   templateUrl: './list-places.component.html',
-  styleUrls: ['./list-places.component.scss']
+  styleUrls: ['./list-places.component.scss'],
 })
-export class ListPlacesComponent {
+export class ListPlacesComponent implements OnInit, OnChanges {
+  places?: PlaceResponse[] = [];
+  @Input() currentTrip?: TripResponse;
 
+  constructor(private placeService: PlacesService) {}
+  ngOnChanges(changes: SimpleChanges): void {
+    this.places = [];
+    this.getPlaces();
+  }
+  ngOnInit(): void {
+    this.places = [];
+    this.getPlaces();
+  }
+
+  getPlaces() {
+    if (this.currentTrip) {
+      this.placeService
+        .getPlacesOfTrip(this.currentTrip.id)
+        .subscribe((response) => {
+          console.log(response);
+          response.forEach((place) => this.places?.push(place));
+        });
+    }
+  }
 }
