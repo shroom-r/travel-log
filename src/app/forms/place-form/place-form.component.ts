10f321510d7e43617f9c88e63ac865cbdb1f9909
Alexander Mussitelli <alexander.mussitelli@gmail.com>
2023-07-08 16:21:59 +0200

    work on create place. form, page, link for buttons

---
+++ src/app/forms/place-form/place-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-place-form',
+  templateUrl: './place-form.component.html',
+  styleUrls: ['./place-form.component.scss']
+})
+export class PlaceFormComponent {
+
+}
