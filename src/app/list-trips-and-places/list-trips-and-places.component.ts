9ef90cf04f6b541e7b4c1cede2a63024893bbb3c
Rafael T <68326435+shroom-r@users.noreply.github.com>
2023-07-13 17:05:22 +0200

    Search feature working fine. Retrieves all trips and places (and trips of retrieved places)

--- src/app/list-trips-and-places/list-trips-and-places.component.ts
+++ src/app/list-trips-and-places/list-trips-and-places.component.ts
@@ -2,7 +2,6 @@ import { Component, Input, OnInit } from '@angular/core';
 import { TripResponse } from '../trips/trip-response.model';
 import { PlaceResponse } from '../places/place-response.model';
 import { TripService } from '../trips/trip.service';
-import { AuthService } from '../auth/auth.service';
 import { PlacesService } from '../places/places.service';
 import { Observable } from 'rxjs';
 
@@ -21,25 +20,23 @@ export class ListTripsAndPlacesComponent implements OnInit {
   placesList: PlaceResponse[] = [];
   placesByTrip: PlacesByTrip[] = [];
 
-  @Input() searchValues?: Observable<string>;
+  @Input() searchValuesObservable?: Observable<string>;
 
   currentlySearchedValues?: string;
 
   constructor(
     private tripService: TripService,
-    private auth: AuthService,
     private placesService: PlacesService
   ) {
     // this.getTripsList();
   }
   ngOnInit(): void {
-    this.searchValues?.subscribe((response) => {
+    this.searchValuesObservable?.subscribe((response) => {
       this.getSearchedTripsAndPlaces(response);
     });
   }
 
   getSearchedTripsAndPlaces(searchValues: string) {
-    debugger;
     //If the new searchValues is different thant the one stored, it means it's a new search so we can clear the array containing service responses
     if (searchValues !== this.currentlySearchedValues) {
       this.tripsList = [];
@@ -49,10 +46,12 @@ export class ListTripsAndPlacesComponent implements OnInit {
       this.tripService
         .searchTripBySearchValue(searchValues)
         .subscribe((response) => {
-          // for (let trip of response) {
-          //   this.tripsList.push(trip);
-          // }
-          this.mapTripsToPlacesByTrip(response);
+          for (let trip of response) {
+            this.tripsList.push(trip);
+          }
+          console.log(this.tripsList);
+          this.mapTrips();
+          // this.mapTripsToPlacesByTrip(response);
         });
       this.placesService
         .searchPlaceBySearchValue(searchValues)
@@ -60,6 +59,9 @@ export class ListTripsAndPlacesComponent implements OnInit {
           for (let place of response) {
             this.placesList.push(place);
           }
+          console.log(this.placesList);
+          this.mapPlaces();
+          // this.mapPlacesToPlacesByTrip(response);
         });
     }
   }
@@ -83,6 +85,49 @@ export class ListTripsAndPlacesComponent implements OnInit {
     //     this.mapPlacesToPlacesByTrip(places);
     //   });
     // }
+  }
+
+  mapTrips() {
+    debugger;
+    do {
+      var tripId = this.tripsList[0].id;
+      //check if trip exists (indexOf returns -1 if not add it)
+      var index = this.placesByTrip.map((el) => el.trip?.id).indexOf(tripId);
+      if (index < 0) {
+        this.placesByTrip.push({ trip: this.tripsList[0] });
+      }
+      //Remove trip from tripsList
+      var indexTripsList = this.tripsList.map((el) => el.id).indexOf(tripId);
+      this.tripsList.splice(indexTripsList, 1);
+    } while (this.tripsList.length);
+  }
+
+  mapPlaces() {
+    debugger;
+    do {
+      var tripId = this.placesList[0].tripId;
+      //check if trip exists (indexOf returns -1 if not get it from API, add it and start mapPlaces again)
+      var index = this.placesByTrip.map((el) => el.trip?.id).indexOf(tripId);
+      if (index < 0) {
+        this.tripService.getTripById(tripId).subscribe((response) => {
+          this.tripsList.push(response);
+          this.mapTrips();
+          this.mapPlaces();
+        });
+        break;
+      } else {
+        if (!this.placesByTrip[index].places) {
+          this.placesByTrip[index].places = [];
+        }
+        //adds places to corresponding trip
+        this.placesByTrip[index].places?.push(this.placesList[0]);
+        //Remove place from placesList
+        var indexPlacesList = this.placesList
+          .map((el) => el.id)
+          .indexOf(this.placesList[0].id);
+        this.placesList.splice(indexPlacesList, 1);
+      }
+    } while (this.placesList.length);
   }
 
   mapTripsToPlacesByTrip(trips: TripResponse[]) {
@@ -110,14 +155,22 @@ export class ListTripsAndPlacesComponent implements OnInit {
       //For each place in placesArray
       //Get trip id
       tripId = place.tripId;
-      //get index of corresponding trip
+
+      //get index of corresponding trip. If index < 0 (means trip doesn't exist) we get it from API
       index = this.placesByTrip.map((el) => el.trip?.id).indexOf(tripId);
       //create array for places if it doesn't exist
-      if (!this.placesByTrip[index].places) {
-        this.placesByTrip[index].places = [];
+      if (index < 0) {
+        this.tripService.getTripById(tripId).subscribe((response) => {
+          this.mapTripsToPlacesByTrip([response]);
+          this.mapPlacesToPlacesByTrip([place]);
+        });
+      } else {
+        if (!this.placesByTrip[index].places) {
+          this.placesByTrip[index].places = [];
+        }
+        //adds places to corresponding trip
+        this.placesByTrip[index].places?.push(place);
       }
-      //adds places to corresponding trip
-      this.placesByTrip[index].places?.push(place);
     }
   }
 }
